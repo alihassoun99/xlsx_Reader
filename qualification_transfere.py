a4547b949ec493ab4d8773d0240e6ca3bd617a72
Ali Hassoun <alihassoun060@gmail.com>
2021-11-23 09:05:11 -0500

    Read Cells and format them

--- qualification_transfere.py
+++ qualification_transfere.py
@@ -30,17 +30,39 @@ cell = "A" + str(2)
 print("cell : ", cell)
 print("cell type : ", type(cell))
 print("---------------------")
-couter = 0
 
 
 ###################################################################
 #
-# passer cellule par cellule et chercher un mot clefs spepicifique
+# passer cellule par cellule et chercher un mot clefs specifique
 #
 ###################################################################
+
+# create an empty array to store the wanted phrase
+
+array = [""]
+
+# loop
 for row in activeSheet.iter_rows(max_row=rowNb):
     for cell in row:
-        print("cell.value = ", cell.value)
-    print(" ") 
+        cellValue = cell.value
+        if(cellValue != None):
+            word_index = cellValue.find('Qualification')
+            if (word_index != -1):
+                # it is a phrase that we want
+                # print("cell.value = ", cellValue)
+                array.append(cellValue)
 
-    
+            else:
+                pass
+
+
+
+# Formatting array's phrases to the wanted form
+formatted_array = []
+for phrases in array:
+    #print(phrases[16:-1])
+    formatted_phrase = phrases[16:-1]
+    formatted_array.append(formatted_phrase)
+
+print(formatted_array)
